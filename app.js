2a240b3972ed7f4ea11a344d299037cc2d3ae8e3
tuyendepzai <letuyenkhtn212@gmail.com>
2023-06-04 02:00:31 +0700

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize');
+const productRouter = require('./routes/productRoutes')
+const userRouter = require('./routes/userRoutes')
+const cartRouter = require('./routes/cartRoutes')
+const bookingRouter = require('./routes/bookingRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
+const chatRouter = require('./routes/chatRoutes')
+const app = express()
+app.use(
+    cors({
+      origin: [
+        "https://myway-shop-app.onrender.com",
+        "http://localhost:3000"
+      ]
+    })
+  );
+app.use(express.static('public/image'));
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(mongoSanitize());
+app.use('/myway/api/products', productRouter)
+app.use('/myway/api/users' , userRouter)
+
+app.use('/myway/api/carts' , cartRouter)
+app.use('/myway/api/bookings' , bookingRouter)
+app.use('/myway/api/reviews' , reviewRouter)
+app.use('/myway/api/chats' , chatRouter)
+module.exports = app
